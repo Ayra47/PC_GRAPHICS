01e068982856559b88a8f07b6e1a5ed7b0502e11
Ayra47 <mr.space213@mail.ru>
2022-03-05 18:19:14 +0500

    laba1
    картинка, квадрат, линия, появление квадрата по нажатию кнопки, удаление всего нарисованного, изменение размера фото и перетаскивание

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,42 @@ namespace ПК_ГРАФИКА_ЛАБА1
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ПК_ГРАФИКА_ЛАБА1.Properties.Resources.meh_i_lapi_pandi;
+            this.pictureBox1.Location = new System.Drawing.Point(541, 174);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(179, 170);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,14 @@ namespace ПК_ГРАФИКА_ЛАБА1
             InitializeComponent();
         }
         Graphics g;
+
+        Control draggedPiece = null;
+        bool resizing = false;
+        private Point startDraggingPoint;
+        private Size startSize;
+        Rectangle rectProposedSize = Rectangle.Empty;
+        int resizingMargin = 5;
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -43,5 +51,87 @@ namespace ПК_ГРАФИКА_ЛАБА1
                     break;
             }
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            draggedPiece = sender as Control;
+
+            if ((e.X <= resizingMargin) || (e.X >= draggedPiece.Width - resizingMargin) ||
+                (e.Y <= resizingMargin) || (e.Y >= draggedPiece.Height - resizingMargin))
+            {
+                resizing = true;
+
+                this.Cursor = Cursors.SizeNWSE;
+
+                this.startSize = new Size(e.X, e.Y);
+                Point pt = this.PointToScreen(draggedPiece.Location);
+                rectProposedSize = new Rectangle(pt, startSize);
+
+                ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+            }
+            else
+            {
+                resizing = false;
+                this.Cursor = Cursors.SizeAll;
+            }
+
+            this.startDraggingPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggedPiece != null)
+            {
+                if (resizing)
+                {
+                    if (rectProposedSize.Width > 0 && rectProposedSize.Height > 0)
+                        ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+                    rectProposedSize.Width = e.X - this.startDraggingPoint.X + this.startSize.Width;
+                    rectProposedSize.Height = e.Y - this.startDraggingPoint.Y + this.startSize.Height;
+                    if (rectProposedSize.Width > 0 && rectProposedSize.Height > 0)
+                        ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+                }
+                else
+                {
+                    Point pt;
+                    if (draggedPiece == sender)
+                        pt = new Point(e.X, e.Y);
+                    else
+                        pt = draggedPiece.PointToClient((sender as Control).PointToScreen(new Point(e.X, e.Y)));
+
+                    draggedPiece.Left += pt.X - this.startDraggingPoint.X;
+                    draggedPiece.Top += pt.Y - this.startDraggingPoint.Y;
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (resizing)
+            {
+                if (rectProposedSize.Width > 0 && rectProposedSize.Height > 0)
+                {
+                    ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+                }
+                if (rectProposedSize.Width > 10 && rectProposedSize.Height > 10)
+                {
+                    this.draggedPiece.Size = rectProposedSize.Size;
+                }
+                else
+                {
+                    this.draggedPiece.Size = new Size((int)Math.Max(10, rectProposedSize.Width), Math.Max(10, rectProposedSize.Height));
+                }
+            }
+
+            this.draggedPiece = null;
+            this.startDraggingPoint = Point.Empty;
+            this.Cursor = Cursors.Default;
+        }
     }
-}
+    }
+
